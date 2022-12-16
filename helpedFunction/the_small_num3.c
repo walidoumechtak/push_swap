69b602d9d2ee4b8b432bdab4be5dbc945eebdd62
Walid Oumechtak <woumecht@e1r1p3.1337.ma>
2022-12-16 22:14:19 +0100

    push swap

--- helpedFunction/the_small_num3.c
+++ helpedFunction/the_small_num3.c
@@ -1,37 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   the_big_num3.c                                     :+:      :+:    :+:   */
+/*   the_small_num3.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: woumecht <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/16 17:51:58 by woumecht          #+#    #+#             */
-/*   Updated: 2022/12/16 22:12:23 by woumecht         ###   ########.fr       */
+/*   Created: 2022/12/16 22:05:37 by woumecht          #+#    #+#             */
+/*   Updated: 2022/12/16 22:12:46 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-int the_big_num3(s_swap *list, int ac)
+int the_small_num3(s_swap *list, int ac)
 {
     s_swap  *ptr;
-    int max_index;
+    int min_index;
     int i;
-    int max;
-    
+    int min;
+
     i = 2;
-    max_index = 2;
-    max = list -> data;
+    min = list -> data;
     list = list -> next;
     ptr = list;
-    while (ptr != NULL && i--)
+    while (list != NULL && i--)
     {
-        if (ptr -> data > max)
+        if (ptr -> data < min)
         {
-            max_index = i;
-            max = ptr -> data;
+            min_index = i;
+            min = ptr -> data;
         }
         ptr = ptr -> next;
     }
-    return (max_index);
+    return (min_index);
 }
