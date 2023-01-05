063bced019c49dda062cd42932c70a8fae780e51
Walid Oumechtak <woumecht@e1r1p12.1337.ma>
2023-01-05 08:19:12 +0100

    push swap

--- fill_stack.c
+++ fill_stack.c
@@ -6,23 +6,24 @@
 /*   By: woumecht <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 15:44:25 by woumecht          #+#    #+#             */
-/*   Updated: 2023/01/04 16:01:52 by woumecht         ###   ########.fr       */
+/*   Updated: 2023/01/05 08:18:59 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    fill_stack(s_swap **head, char **av, int ac)
+void	fill_stack(s_swap **head, char **av, int ac)
 {
-    int i;
-    int cpt;
-    i = 2;
-		cpt = ac - 2;
-		(*head) = lst_new(ft_atoi(av[1]));
-		while (cpt > 0)
-		{
-			add_back(head, lst_new(ft_atoi(av[i])));
-			i++;
-			cpt--;
-		}
+	int	i;
+	int	cpt;
+
+	i = 2;
+	cpt = ac - 2;
+	(*head) = lst_new(ft_atoi(av[1]));
+	while (cpt > 0)
+	{
+		add_back(head, lst_new(ft_atoi(av[i])));
+		i++;
+		cpt--;
+	}
 }
