c42a95e3247d950c9beb8399e0afade2e709395f
Walid Oumechtak <woumecht@e2r1p12.1337.ma>
2023-01-06 06:03:09 +0100

    push swap

--- test.c
+++ test.c
@@ -5,8 +5,36 @@
 #include "push_swap.h"
 
 
+void moveZeroes(int* nums, int numsSize){
+    int i, size, temp;
+
+    i = 0;
+    while (nums[numsSize - 1] != 0 || nums[numsSize - 2] != 0)
+    {
+        if (nums[i] == 0 && i < numsSize - 1)
+        {
+            temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
+        }
+        i++;
+        if (i == numsSize - 1)
+            i = 0;
+    }
+}
+
 int main(int ac, char **av) 
 {
+
+    int nums[] = {0,1,0,3,12};
+    moveZeroes(nums, 5);
+
+    int i = 0;
+    while (i<5)
+    {
+        printf("%d\n", nums[i]);
+        i++;
+    }
     // s_swap  *f;
     // s_swap  *s;
     // s_swap  *t;
@@ -28,24 +56,24 @@ int main(int ac, char **av)
     //     f = f -> next;
     // }
 
-    int i = 2;
-    int num = atoi(av[1]);
+    // int i = 2;
+    // int num = atoi(av[1]);
 
-    if (ac == 2)
-    {
-        while (i <= num)
-        {
-            if(num % i == 0)
-            {
-                printf("%d", i);
-                if (i == num)
-                    break;
-                printf("*");
-                num /= i;
-                i = 2;
-            }
-            i++;
-        }
-    }
+    // if (ac == 2)
+    // {
+    //     while (i <= num)
+    //     {
+    //         if(num % i == 0)
+    //         {
+    //             printf("%d", i);
+    //             if (i == num)
+    //                 break;
+    //             printf("*");
+    //             num /= i;
+    //             i = 2;
+    //         }
+    //         i++;
+    //     }
+    // }
     return 0;
 }
